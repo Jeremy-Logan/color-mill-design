fc9493c2f0bfaebd67e04cd417c2f4ca02d814ac
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-03-28 12:45:14 -0700

    changed hexagons to svg

--- components/hexGrid.js
+++ components/hexGrid.js
@@ -7,20 +7,20 @@ import {
 } from 'framer-motion'
 
 export default function HexGrid() {
-const delayArray=[0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0,20]
+const delayArray=[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20]
 
 	return (
 		<div className='w-screen overflow-hidden z-0' >
-			<motion.div className='flex '>
+			<motion.div className='flex'>
 				{delayArray.map(delay=> <Hexagon delay={delay}/>)}
 			</motion.div>
-			<motion.div className='flex ml-[-60px] -mt-8'>
+			<motion.div className='flex transform translate-x-12 -mt-3'>
+				{delayArray.map(delay=> <Hexagon  delay={delay}/>)}
+			</motion.div>
+            <motion.div className='flex -mt-3'>
 				{delayArray.map(delay=> <Hexagon delay={delay}/>)}
 			</motion.div>
-            <motion.div className='flex -mt-8'>
-				{delayArray.map(delay=> <Hexagon delay={delay}/>)}
-			</motion.div>
-            <motion.div className='flex ml-[-60px] -mt-8'>
+            <motion.div className='flex transform translate-x-12 -mt-3'>
 				{delayArray.map(delay=> <Hexagon delay={delay}/>)}
 			</motion.div>
 		</div>
