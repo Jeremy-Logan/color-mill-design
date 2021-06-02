4d0f21415aeb7666a0a2d975a52732e764861e5d
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-06-02 08:07:13 -0700

    Capabilities Section tweaks

--- components/capabilitiesSection.js
+++ components/capabilitiesSection.js
@@ -14,7 +14,7 @@ import CapabilitiesCard from './capabilitiesCard'
 
 const SlideIn = ({ children }) => {
 	const controls = useAnimation()
-	const [ref, inView] = useInView()
+	const [ref, inView] = useInView({ delay: 500})
 
 	useEffect(() => {
 		inView ? controls.start('visible') : controls.start('hidden')
@@ -28,7 +28,7 @@ const SlideIn = ({ children }) => {
 			initial='hidden'
 			
 			variants={{
-				visible: { opacity: 1, x: 1, transition:{delay: 0.05} },
+				visible: { opacity: 1, x: 1, transition:{delay: 0.25} },
 				hidden: { opacity: 0, x: -100 },
 			}}
 			>
@@ -58,7 +58,7 @@ const SlideUp = ({ children }) => {
 			ref={ref}
 			animate={controls}
 			initial='hidden'
-			transition={{ staggerChildren: 0.1, delay: 1}}
+			transition={{ staggerChildren: 0.1, delay: 1.25}}
 			>
 			{children}
 		</motion.div>
@@ -75,7 +75,7 @@ export default function CapabilitiesSection() {
 							<h3 className='text-5xl font-bold mt-36 z-20 '>
 								Our Capabilities
 							</h3>
-							<h4 className='text-2xl font-bold mt-8 mb-24 z-20 '>
+							<h4 className='text-2xl mt-8 mb-24 z-20 '>
 								We love crafting beautiful, smart and inspired
 								work for businesses we believe in.
 							</h4>
@@ -149,35 +149,36 @@ export default function CapabilitiesSection() {
 							<SlideUp>
 							<div className='flex flex-row justify-between mb-12 mx-[10vw]'>
 								<motion.h4 variants={stagger} className=' text-3xl text-[#2fa7d9] mb-8'>
-									Discover. 
-								</motion.h4>
+									Discover.
+								</motion.h4> <motion.img variants={stagger} className='transform scale-125 -mt-6 center' src='/arrows-BlueToOrange.svg'/>
 								<motion.h4 variants={stagger} className=' text-3xl text-[#FF7A2E] mb-8'>
 									Define. 
-								</motion.h4>
+								</motion.h4><motion.img variants={stagger} className='transform scale-125 -mt-6 center' src='/arrows-OrangeToRed.svg'/>
 								<motion.h4 variants={stagger} className=' text-3xl text-[#FF006A] mb-8'>
 									Do. 
-								</motion.h4>
+								</motion.h4><motion.img variants={stagger} className='transform scale-125 -mt-6 center' src='/arrows-RedToGreen.svg'/>
 								<motion.h4 variants={stagger} className=' text-3xl text-[#41c71f] mb-8'>
 									Deliver!
 								</motion.h4>
 							</div></SlideUp>
+							<SlideUp>
 							<div className='grid gap-12 grid-cols-2'>
-							<p className='mb-2 mx-24'>
+							<motion.p variants={stagger} className='mb-2 mx-24 p-10 shadow-md bg-[#f0f8ff]'>
 								<h4 className='font-bold text-xl text-[#2fa7d9]'>Discover:</h4>{' '}
 								This is the part where we get to know you. We
 								ask questions to get at the heart of your
 								business and start to define what it is that
 								makes your business special.
-							</p>
-							<p className='mb-2 mx-24'>
+							</motion.p>
+							<motion.p variants={stagger} className='mb-2 mx-24 p-10 shadow-md bg-[#fcf7f1]'>
 								<h4 className='font-bold text-xl text-[#FF7A2E]'>Define:</h4>We
 								put the user at the center of everything we
 								create: prioritizing their needs, defining the
 								scope with them in mind, choosing the project
 								direction and details based on what will work
 								for them and for your business.
-							</p>
-							<p className='mb-2 mx-24'>
+							</motion.p>
+							<motion.p variants={stagger} className='mb-2 mx-24 p-10 shadow-md bg-[#fdf3f7]'>
 								<h4 className='font-bold text-xl text-[#FF006A]'>Do:</h4>We get
 								to work! You may not hear from us for a while in
 								this stage. We have collected and defined, now
@@ -185,12 +186,12 @@ export default function CapabilitiesSection() {
 								will check in with you to make sure we’re still
 								on the right track. Once we all agree it’s ready
 								for the consumer’s hands, we...
-							</p>
-							<p className='mb-2 mx-24'>
+							</motion.p>
+							<motion.p variants={stagger} className='mb-2 mx-24 p-10 shadow-md bg-[#f0fff2]'>
 								<h4 className='font-bold text-xl text-[#41c71f]'>Deliver!:</h4>
 								The product is done and out in the world!
-							</p>
-						</div>
+							</motion.p>
+						</div></SlideUp>
 					</div>
 				</div>
 			</div>
