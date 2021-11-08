b656886ae4c45c2187bb5cf3517cc612b78d814a
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-11-08 13:07:28 -0800

    styling blog page

--- pages/news.js
+++ pages/news.js
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/Link'
+import Image from 'next/image'
 import client from '../client'
+import imageUrlBuilder from '@sanity/image-url'
 import groq from 'groq'
+
+function urlFor(source) {
+	return imageUrlBuilder(client).image(source)
+}
 
 const News = (props) => {
 	const { posts = [] } = props
@@ -13,21 +19,56 @@ const News = (props) => {
 			</Head>
 
 			<main>
-				<h1 className='text-6xl'>News</h1>
-				{posts.map(
-					({ _id, title = '', slug = '', publishedAt = '' }) =>
-						slug && (
-							<li key={_id}>
-								<Link
-									prefetch
-									href='/post/[slug]'
-									as={`/post/${slug.current}`}>
-									<a>{title}</a>
-								</Link>{' '}
-								({new Date(publishedAt).toDateString()})
-							</li>
-						)
-				)}
+				<h1 className='text-6xl font-serif mt-36 mx-[15vw]'>News</h1>
+				<div className='grid md:grid-cols-3 grid-cols-1 gap-6 mx-auto w-[80vw] mt-36'>
+					{posts.map(
+						({
+							_id,
+							title = '',
+							slug = '',
+							publishedAt = '',
+							mainImage = '',
+							excerpt = '',
+						}) =>
+							slug && (
+								<li
+									key={_id}
+									className='list-none flex flex-col mb-12'>
+									<Link
+										
+										href='/post/[slug]'
+										as={`/post/${slug.current}`}>
+										<img
+											className='h-96 cursor-pointer'
+											src={urlFor(mainImage)
+												.width(800)
+												.url()}
+										/>
+									</Link>{' '}
+									<Link
+										
+										href='/post/[slug]'
+										as={`/post/${slug.current}`}>
+										<a className='text-2xl font-semibold mx-4 mt-2'>
+											{title}
+										</a>
+									</Link>{' '}
+									<span className='mx-4 my-2 text-sm'>
+										{new Date(publishedAt).toDateString()}
+									</span>
+									<p className='mx-4'>
+										{excerpt}
+										<Link 
+											
+											href='/post/[slug]'
+											as={`/post/${slug.current}`}>
+											<a className="text-['#7700FF'] underline ml-2">Read more...</a>
+										</Link>
+									</p>
+								</li>
+							)
+					)}
+				</div>
 			</main>
 		</div>
 	)
