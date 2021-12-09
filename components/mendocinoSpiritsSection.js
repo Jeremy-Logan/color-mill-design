153e48d05167096cad24844675928ac94327ef8f
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-12-08 18:16:18 -0800

    .

--- components/mendocinoSpiritsSection.js
+++ components/mendocinoSpiritsSection.js
@@ -74,7 +74,7 @@ export default function MendocinoSpiritsSection() {
 									quality={100}
 									src='r_max/v1638489233/Color%20Mill%20Design/Mendocino-Spirits-3-Bottles_i8oym6.jpg'
 									placeholder='blur'
-									blurDataURL='ar_1:1,bo_0px_solid_rgb:ffffff,c_fill,co_rgb:ffffff,fl_alpha.preserve_transparency,g_auto,o_100,r_max,w_600/t_placeholder/v1638489233/Color%20Mill%20Design/Mendocino-Spirits-3-Bottles_i8oym6.jpg'
+									blurDataURL='https://res.cloudinary.com/the-color-mill/image/upload/t_placeholder/v1638489233/Color%20Mill%20Design/Mendocino-Spirits-3-Bottles_i8oym6.jpg'
 								/>
 							</Link>
 						</motion.button>
